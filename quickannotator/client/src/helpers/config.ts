b4caff15e4bd4893d9a0baa8a74ebadec70b7180
jackson <jacksonjjacobs@gmail.com>
2025-02-20 19:02:32 +0000

    refactor config to use enums for TOOLBAR_KEYS and LAYER_KEYS

--- quickannotator/client/src/helpers/config.ts
+++ quickannotator/client/src/helpers/config.ts
@@ -1,18 +1,18 @@
-export const TOOLBAR_KEYS = Object.freeze({
-    POINTER: '0',
-    IMPORT: '1',
-    BRUSH: '2',
-    WAND: '3',
-    ERASER: '4',
-    POLYGON: '5',
-})
+export enum TOOLBAR_KEYS {
+    POINTER = '0',
+    IMPORT = '1',
+    BRUSH = '2',
+    WAND = '3',
+    ERASER = '4',
+    POLYGON = '5',
+}
 
-export const LAYER_KEYS = Object.freeze({
-    GT: 0,
-    PRED: 1,
-    OSM: 2,
-    ANN: 3
-})
+export enum LAYER_KEYS {
+    GT = 0,
+    PRED,
+    OSM,
+    ANN,
+}
 
 export enum TILE_STATUS {
     UNSEEN = 0,
