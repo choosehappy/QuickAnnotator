8eeb801ab2701e35587c291a994be98e96ee5115
jackson <jacksonjjacobs@gmail.com>
2025-04-04 04:01:53 +0000

    Add logging functionality with SQLAlchemy integration and tests

---
+++ quickannotator/db/logging.py
@@ -0,0 +1,27 @@
+from quickannotator.db import get_session
+import logging
+import quickannotator.db.models as models
+from datetime import datetime
+
+class SQLAlchemyHandler(logging.Handler):
+    def emit(self, record):
+        with get_session() as db_session:
+            log_entry = models.Log(
+                name=record.name,
+                timestamp=datetime.fromtimestamp(record.created),
+                level=record.levelname,
+                message=self.format(record)
+            )
+
+            db_session.add(log_entry)
+
+
+def init_logger(logger_name) -> logging.Logger:
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    handler = SQLAlchemyHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
