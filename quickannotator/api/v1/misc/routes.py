72ba0b7b32cf6b914aee85ecdb9d2b2eaeb315ad
jackson <jacksonjjacobs@gmail.com>
2025-05-07 18:23:58 +0000

    Refactor ERD endpoint to use NamedTemporaryFile. Create test for ERD endpoint

--- quickannotator/api/v1/misc/routes.py
+++ quickannotator/api/v1/misc/routes.py
@@ -3,15 +3,16 @@ from flask.views import MethodView
 from quickannotator.db import Base
 from eralchemy import render_er
 from flask import send_file
+import tempfile
 
 bp = Blueprint('misc', __name__, description='Miscellaneous operations')
 
 @bp.route('/erd')
 class ERD(MethodView):
     def get(self):
-        tmp_filepath = '/tmp/erd.png'
-        # Generate the ER diagram using eralchemy
-        # NOTE: Unfortunately eralchemy does not support in-memory rendering e.g., streams
-        render_er(Base.metadata, tmp_filepath)  
+        with tempfile.NamedTemporaryFile(suffix='.png', delete=False) as tmp_file:
+            tmp_filepath = tmp_file.name
+            # Generate the ER diagram using eralchemy
+            render_er(Base.metadata, tmp_filepath)
 
         return send_file(tmp_filepath, mimetype='image/png')
