9ba83f329746a0c05d5aca016f31e11496311789
jackson <jacksonjjacobs@gmail.com>
2025-05-22 14:37:58 +0000

    Update legend component positioning and size.

--- quickannotator/client/src/components/legend.tsx
+++ quickannotator/client/src/components/legend.tsx
@@ -4,24 +4,20 @@ import Card from "react-bootstrap/Card";
 interface Props {
     mouseCoords: { x: number, y: number };
 }
-
 const Legend = React.memo((props: Props) => {
 
     return (
         <Card style={{
             position: "absolute",
-            bottom: 10,
-            right: 10,
+            bottom: 0,
+            right: 0,
             backgroundColor: "rgba(255, 255, 255, 0.8)",
             borderColor: "rgba(0, 0, 0, 0.8)",
             borderRadius: 6,
             zIndex: 10,
         }}>
-            <Card.Header>
-                <h6 style={{ margin: 0 }}>Legend</h6>
-            </Card.Header>
-            <Card.Body>
-                <div style={{ marginTop: "10px", fontSize: "0.9em", color: "gray" }}>
+            <Card.Body style={{ padding: "0.2em" }}>
+                <div style={{ fontSize: "0.9em", color: "gray" }}>
                     x: {props.mouseCoords.x}, y: {props.mouseCoords.y}
                 </div>
             </Card.Body>
